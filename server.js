2a1ac437c8da1996241cf34c80ce753966f732e6
Abuye Mamuye <abuyeye3@gmail.com>
2021-02-18 15:33:13 -0800

    create require and port

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const PORT = process.env.PORT || 3000;
 
 
 
-// const express = require("express");
-// const mongoose = require("mongoose");
 
-// const PORT = process.env.PORT || 3000;
+
 
 // const app = express();
 
