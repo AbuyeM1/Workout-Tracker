0fd648cdbbfb401b5a5335bba8bfc943ee149c50
Abuye Mamuye <abuyeye3@gmail.com>
2021-02-18 15:57:30 -0800

    connecting to MongoDB and Add the routes here

--- server.js
+++ server.js
@@ -21,19 +21,25 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
+//connecting to MongoDB
+mongoose.connect(MONGODB_URI, {
+  
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+//Add the routes here
+app.use(require("./routes/api.js"));
+
+app.use(require("./routes/view.js"));
+
+app.listen(PORT, function () {
+    console.log("App now Listening On: http://localhost:" + PORT);
+});
 
 
 
 
 
 
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-
-// // routes
-// app.use(require("./routes/api.js"));
-
-// app.listen(PORT, () => {
-//   console.log(`App running on port ${PORT}!`);
-// });
