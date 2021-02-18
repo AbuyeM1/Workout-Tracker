0faa14d18cecfeae5091a52062023534c237d642
Abuye Mamuye <abuyeye3@gmail.com>
2021-02-18 15:40:53 -0800

    add. code on the server.js

--- server.js
+++ server.js
@@ -3,19 +3,30 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const PORT = process.env.PORT || 3000;
 
+const User = require("./models/userModel.js");
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
 
 
 
 
-// const app = express();
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
 
-// app.use(express.static("public"));
-
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 //   useNewUrlParser: true,
 //   useFindAndModify: false
 // });
