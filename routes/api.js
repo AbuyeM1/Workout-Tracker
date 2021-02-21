3ef842f1cf1b6062bcc0d2982d4f6ca155579ee2
Abuye Mamuye <abuyeye3@gmail.com>
2021-02-20 20:22:21 -0800

    add code

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,26 @@
 const router = require("express").Router();
 const User = require("../models/userModel.js");
 
+
 router.get("/api/workouts/range", function (req, res) {
     User.find().sort({ day: -1 }).limit(7).then(function (data) {
         res.json(data)
     })
 });
 
+
 router.get("/api/workouts", function (req, res) {
     User.aggregate([
         {
             $addFields: {
-                totalDuration: {$sum: "$exercise.duration" }
+                totalDuration: {$sum: "$exercises.duration" }
             } 
         }
     ]).then(function (data) {
         res.json(data)
     })
 });
+
 
 router.post("/api/workouts", function (req, res) {
     console.log(req.body);
@@ -28,10 +31,11 @@ router.post("/api/workouts", function (req, res) {
     })
 });
 
+
 router.put("/api/workouts/:id", function (req, res) {
 
     console.log("Here are the body", req.body);
-    User.findByIdAndUpdate(req.params.id, { exercises: req.body }).then(function (workout) {
+    User.findByIdAndUpdate({_id:req.params.id},{$push:{ exercises: req.body }},{upsert:true,new:true} ).then(function (workout) {
         return res.json(workout)
     }).catch(function (err) {
         res.json(err);
